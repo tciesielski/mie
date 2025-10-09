fe62a524bcb3a8d19da5163102346b3f2fb176a0
Tomasz Ciesielski <tciesielski@M1Tomasz.local>
2025-10-09 15:19:05 +0200

    Initial commit

---
+++ packages/container/components/MarketingApp.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useRef } from "react";
+import { mount } from "marketing/MarketingApp";
+const MarketingApp = () => {
+    const content = useRef(null);
+
+    useEffect(() => {
+        if (content.current) {
+            mount(content.current);
+            console.log(mount);
+        }
+    }, [content]);
+
+    return (
+        <div>test
+            <div ref={content} />
+        </div>
+    );
+};
+export default MarketingApp;
---
+++ packages/container/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+import MarketingApp from "../components/MarketingApp";
+
+const App = () => {
+    return (
+        <div>
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/"></Route>
+                    <Route exact path="/marketing">
+                    </Route>
+                </Switch>
+            Container
+            <Link to="/marketing">Marketing</Link>
+                        <MarketingApp />
+            </BrowserRouter>
+        </div>
+    );
+};
+
+export default App;
