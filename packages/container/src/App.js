190c5bdb52cf0444ab6534e293bf15e343acab2d
Tomasz Ciesielski <taciesielski@gmail.com>
2025-10-11 17:13:19 +0200

    CSS namespaces fix

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,21 +1,28 @@
 import React from "react";
 import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles";
 import MarketingApp from "../components/MarketingApp";
 import Header from "../components/Header";
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co'
+});
 
 const App = () => {
     return (
         <BrowserRouter>
-            <div>
-                <Switch>
-                    <Route exact path="/"></Route>
-                    <Route exact path="/marketing">
-                    </Route>
-                </Switch>
-                {/* <Link to="/marketing">Marketing</Link> */}
-                <Header />
-                <MarketingApp />
-            </div>
+            <StylesProvider generateClassName={generateClassName}>
+                <div>
+                    <Switch>
+                        <Route exact path="/"></Route>
+                        <Route exact path="/marketing">
+                        </Route>
+                    </Switch>
+                    {/* <Link to="/marketing">Marketing</Link> */}
+                    <Header />
+                    <MarketingApp />
+                </div>
+            </StylesProvider>
         </BrowserRouter>
     );
 };
